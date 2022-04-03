21767da19eb0b9b5551c741360edf4ad8b95ed1b
lampros2003 <46668710+lampros2003@users.noreply.github.com>
2022-04-03 11:21:38 +0300

    version 3

--- lab2/lab2_step3_0/main.c
+++ lab2/lab2_step3_0/main.c
@@ -3,28 +3,36 @@
 #define M 10
 #define N 20
 #define timequantum 1
-/*οι μεταβλητες ονομάστηκαν με την εξής λογική: t: temperature , r : right side l:left side t:top b bottom,in inside
-exaple : ttr temperature top right corner
+/*ΞΏΞΉ ΞΌΞµΟ„Ξ±Ξ²Ξ»Ξ·Ο„ΞµΟ‚ ΞΏΞ½ΞΏΞΌΞ¬ΟƒΟ„Ξ·ΞΊΞ±Ξ½ ΞΌΞµ Ο„Ξ·Ξ½ ΞµΞΎΞ®Ο‚ Ξ»ΞΏΞ³ΞΉΞΊΞ®: t: temperature , r : right side l:left side t:top b bottom,in inside
+exaple : tt temperature top
 */
 double tempgetter(void);
 void plakamaker(double ttr,double ttl,double tbr,double tbl, double tin,double plaka[N][M]);
 void textgiver(void);
 void plakaprinter(double plaka[N][M]);
 void plakachanger(double plaka[N][M],int quantum);
+double meanvalue(double oneval ,double twoval);
 int main()
 {
-    double ttr,ttl,tbr,tbl,tin,plaka[N][M];
+    double tr,tt,tb,tl,tin,plaka[N][M];
     textgiver();
+    tt = tempgetter();
+    tr = tempgetter();
 
-    ttr = tempgetter();
-    ttl = tempgetter();
-    tbr = tempgetter();
-    tbl = tempgetter();
+    tb = tempgetter();
+    tl = tempgetter();
     tin = tempgetter();
-    plakamaker( ttr, ttl, tbr, tbl,  tin,plaka);
+    plakamaker( tr, tl, tb, tt,  tin,plaka);
     plakaprinter(plaka);
     plakachanger(plaka,timequantum);
     return 0;
+}
+double meanvalue(double oneval ,double twoval){
+    double mean ;
+    mean = (double)(oneval + twoval)/2;
+    return mean;
+
+
 }
 double tempgetter(void)
 {
@@ -35,33 +43,44 @@ double tempgetter(void)
     return (double)tempgotten;
 
 }
-void plakamaker(double ttr,double ttl,double tbr,double tbl, double tin,double plaka[N][M])
+void plakamaker(double tr,double tl,double tb,double tt, double tin,double plaka[N][M])
 {
 
     int i,j ;
 
-    for(i=0; i<N; i++)
+    for(i=1 ;i<N-1;i++){
+        plaka[i][0]=tl;
+        plaka[i][M-1]=tr;
+        }
+    for(j=1; j<M-1; j++)
+        {
+            plaka[0][j]= tt;
+            plaka[N-1][j] = tb;
+        }
+    for(i=1; i<N-1; i++)
     {
-        for(j=0; j<M; j++)
+
+        for(j=1; j<M-1; j++)
         {
             plaka[i][j]= tin;
         }
     }
-    plaka[0][0]=ttl;
-    plaka[0][M-1]=ttr;
-    plaka[N-1][0]=tbl;
-    plaka[N-1][M-1]=tbr;
+    plaka[N-1][M-1] = meanvalue(tr,tb);
+    plaka[0][0]=meanvalue(tt,tl);
+    plaka[N-1][0]=meanvalue(tb,tl);
+    plaka[0][M-1]=meanvalue(tr,tt);
+
 
     return;
 }
 void textgiver(void){
-    printf("lorem ipsum\n");
+    printf("Please input the temperatures of the sides of the object in clockwise order staring from the top and then input the initial inside temperature of the object \n");
 }
 void plakaprinter(double plaka[N][M] ){
     int i,j;
     for(i=0;i<N;i++){
         for(j=0;j<M;j++){
-            printf("|%.4lf|   ",plaka[i][j]);
+            printf("|%6.4lf|   ",plaka[i][j]);
         }
         printf("\n");
     }
